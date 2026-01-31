0a99b623999ad3ff9fe422a75a0c65126211dac6
citruspunch <andrewtobar05@gmail.com>
2026-01-30 19:54:33 -0600

    Refactor build script to include additional source files for compilation

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -5,16 +5,25 @@ set -e
 
 # Remove previous compiled objects and binaries
 echo "Cleaning up previous build files..."
-rm -f root.o main.o calculadora.elf calculadora.bin
+rm -f root.o main.o os.o stdio.o string.o calculadora.elf calculadora.bin
 
-echo "Assembling startup.s..."
-arm-none-eabi-as -o root.o root.s
+echo "Assembling root.s..."
+arm-none-eabi-as -o root.o drivers/root.s
 
-echo "Compiling main.c..."
-arm-none-eabi-gcc -c -o main.o main.c
+echo "Compiling os.c (OS Level)..."
+arm-none-eabi-gcc -c -o os.o drivers/os.c -I drivers
+
+echo "Compiling stdio.c (Language Library Level)..."
+arm-none-eabi-gcc -c -o stdio.o lib/stdio.c -I lib -I drivers
+
+echo "Compiling string.c (Language Library Level)..."
+arm-none-eabi-gcc -c -o string.o lib/string.c -I lib
+
+echo "Compiling main.c (User Level)..."
+arm-none-eabi-gcc -c -o main.o app/main.c -I lib -I drivers
 
 echo "Linking object files..."
-arm-none-eabi-ld -T linker.ld -o calculadora.elf root.o main.o
+arm-none-eabi-ld -T linker.ld -o calculadora.elf root.o os.o stdio.o string.o main.o
 
 echo "Converting ELF to binary..."
 arm-none-eabi-objcopy -O binary calculadora.elf calculadora.bin
